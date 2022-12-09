0de734ec57c84972c1b1994cc578f0c0799183bd
Iryna <37odri@gmail.com>
2022-12-10 00:08:34 +0200

    initial commit ::octocat::

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss'],
+})
+export class CoursesComponent implements OnInit {
+  courses = [
+    {
+      id: 1,
+      title: 'Angular 13 Fundamentals',
+      description: 'Learn the fundamentals of Angular 13',
+      percentComplete: 26,
+      favorite: true,
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
